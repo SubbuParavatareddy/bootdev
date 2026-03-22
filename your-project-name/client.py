f4891068a71f4b845279c1db535113648007459c
Subbu <paravats@gmail.com>
2026-03-21 23:56:48 -0700

    resolve merge conflict in client.py

--- your-project-name/client.py
+++ your-project-name/client.py
@@ -1,5 +1,8 @@
 from google import genai
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Only run this block for Gemini Developer API
-client = genai.Client(api_key='')
-print(client)
+api_key = os.environ.get('GEMINI_API_KEY')
+client = genai.Client(api_key=api_key)
--- your-project-name/generateContent.py
+++
@@ -1,5 +0,0 @@
-from client import client
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is the sky blue?'
-)
-print(response.text)
